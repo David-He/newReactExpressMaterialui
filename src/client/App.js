670e544d284f588c80c29e401f99ebf404c4b74b
Devid-He <eazyeazy@126.com>
2018-08-26 11:02:26 +0800

    增加了基本样式，可以使Content align client

--- src/client/App.js
+++ src/client/App.js
@@ -1,30 +1,78 @@
 import React, { Component } from "react";
-import "./app.css";
-import ReactImage from './react.png';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default class App extends Component {
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const styles = {
+  root: {
+    display: "flex",
+    "flex-direction": "column",
+    height: "100%"
+  },
+  appbar:{
+    flex:"0 0 auto",
+  },
+
+  content:{
+    flex:"1 0 auto",
+  },
+
+  navbar:{
+    flex: "0 0 auto"
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: null };
-  }
+    
+  };
 
-  componentDidMount() {
-    fetch('api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
+  
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {this.state.username ? (
-          <h1> Hello{' '}{this.state.username}
-          </h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}
-        <img src={ReactImage} alt="react" />
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appbar}>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.content}></div>
+        <BottomNavigation
+          
+          
+          showLabels
+          className={classes.navbar}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
       </div>
     );
   }
-}
+};
+
+export default withStyles(styles)(App);
